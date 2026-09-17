31e9d633f61587d43c5de694e573a8f2b3649ac9
Aaron Renteria <yoshuarenteria05@gmail.com>
2026-09-17 17:18:12 -0600

    Creamos un PHP y lo conectamos con xampp y vimos estructura basica de HTML5

---
+++ index.php
@@ -0,0 +1,37 @@
+<!-- <?php   
+    echo "K rollo";
+?> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1 {
+            /* Texto */
+            color: #00ffea;
+        }
+        div {
+            /* Fondo */
+            background: #ff5100;
+        }
+    </style>
+</head>
+<body>
+    <h1>Titulo</h1>
+    <h1>Hola Crayola</h1>
+    <h2>Hola Crayola</h2>
+    <h3>Hola Crayola</h3>
+    <h4>Hola Crayola</h4>
+    <h5>Hola Crayola</h5>
+    <h6>Hola Crayola</h6>
+    
+    <div>
+        <h2>Titulo del parrafo</h2>
+
+        <p>Este es un parrafo</p>  
+    </div>
+</body>
+</html>
